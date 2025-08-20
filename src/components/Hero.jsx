96b1e8422c9f8cd12ed661c14535539c306884cb
himanshu <himanshu32505@gmail.com>
2025-08-20 13:25:13 +0530

    second commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BackgroundBeams } from './ui/background-beams'
+
+const Hero = () => {
+  return (
+    <div className="h-screen fixed w-full">
+        <BackgroundBeams />
+    </div>
+  )
+}
+
+export default Hero
