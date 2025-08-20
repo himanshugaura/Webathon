dd1e9006dfc5b1703b8b0b206e31c0ecc616d09a
himanshu <himanshu32505@gmail.com>
2025-08-20 16:00:05 +0530

    fix

--- src/data/events.js
+++ src/data/events.js
@@ -2,17 +2,17 @@ export const schoolEventsData = [
   {
     name: "Science Exhibition",
     description: "Explore groundbreaking scientific projects and innovative experiments from students showcasing their research and discoveries.",
-    src: "/events/event1.jpg"
+    src: "/events/event1.JPG"
   },
   {
     name: "Coding Competition",
     description: "A thrilling contest where students showcase their coding skills by solving complex problems within a set time frame.",
-    src: "/events/event2.jpg"
+    src: "/events/event2.JPG"
   },
   {
     name: "Workshop",
     description: "Hands-on learning sessions where participants can dive into topics like robotics, AI, and software development with expert guidance.",
-    src: "/events/event3.jpg"
+    src: "/events/event3.JPG"
   },
 ];
 
@@ -21,27 +21,27 @@ export const collegeEventsData = [
   {
     name: "Workshop",
     description: "Hands-on learning sessions where participants can dive into topics like robotics, AI, and software development with expert guidance.",
-    src: "/events/event3.jpg"
+    src: "/events/event3.JPG"
   },
   {
     name: "Seminar",
     description: "Expert-led discussions and presentations on the latest trends in technology, innovation, and career-building strategies for students and professionals.",
-    src: "/events/event4.jpg"
+    src: "/events/event4.JPG"
   },
   {
     name: "Capture the Flag (CTF)",
     description: "A cybersecurity competition where participants solve security-related challenges to capture virtual flags and test their ethical hacking skills.",
-    src: "/events/event5.jpg"
+    src: "/events/event5.JPG"
   },
   {
     name: "AWS Study Jam",
     description: "An intensive, collaborative session focused on learning Amazon Web Services, where participants can deepen their cloud computing knowledge and skills.",
-    src: "/events/event6.jpg"
+    src: "/events/event6.JPG"
   },
   {
     name: "Hackathon",
     description: "A fast-paced coding marathon where participants work together to develop innovative software solutions or products within a specific theme or problem statement.",
-    src: "/events/event7.jpg"
+    src: "/events/event7.JPG"
   }
 ];
 
@@ -49,46 +49,46 @@ export const outdoorEventsData = [
   {
     name: "Workshop",
     description: "Hands-on learning sessions where participants can dive into topics like robotics, AI, and software development with expert guidance.",
-    src: "/events/event3.jpg"
+    src: "/events/event3.JPG"
   },
   {
     name: "Seminar",
     description: "Expert-led discussions and presentations on the latest trends in technology, innovation, and career-building strategies for students and professionals.",
-    src: "/events/event4.jpg"
+    src: "/events/event4.JPG"
   },
   {
     name: "Capture the Flag (CTF)",
     description: "A cybersecurity competition where participants solve security-related challenges to capture virtual flags and test their ethical hacking skills.",
-    src: "/events/event5.jpg"
+    src: "/events/event5.JPG"
   },
   {
     name: "AWS Study Jam",
     description: "An intensive, collaborative session focused on learning Amazon Web Services, where participants can deepen their cloud computing knowledge and skills.",
-    src: "/events/event6.jpg"
+    src: "/events/event6.JPG"
   },
   {
     name: "Hackathon",
     description: "A fast-paced coding marathon where participants work together to develop innovative software solutions or products within a specific theme or problem statement.",
-    src: "/events/event7.jpg"
+    src: "/events/event7.JPG"
   },
   {
     name: "Stalls",
     description: "A variety of stalls showcasing unique products, games, and food items created by students, offering a fun and engaging experience for all.",
-    src: "/events/event8.jpg"
+    src: "/events/event8.JPG"
   },
   {
     name: "Treasure Hunt",
     description: "An exciting adventure where participants solve clues and riddles to find hidden treasures across the campus, challenging both their wit and teamwork.",
-    src: "/events/event9.jpg"
+    src: "/events/event9.JPG"
   },
   {
     name: "DJ Night",
     description: "An electrifying night of music and dance, with a live DJ spinning tracks, offering students the chance to let loose and party under the stars.",
-    src: "/events/event10.jpg"
+    src: "/events/event10.JPG"
   },
   {
     name: "Cultural Events",
     description: "A vibrant celebration of culture with performances, dances, and music from various regions, bringing together students to showcase their diverse talents.",
-    src: "/events/event11.jpg"
+    src: "/events/event11.JPG"
   }
 ];
