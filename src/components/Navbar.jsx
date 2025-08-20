36664d9225839dc23263d6e088cd6020dc582ece
himanshu <himanshu32505@gmail.com>
2025-08-20 13:54:30 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="w-full p-8 bg-black flex justify-between">
+    <div className="w-full fixed z-10 top-0 px-20 bg-transparent flex justify-between items-center">
         <div>
-            <Image src="/logo_white.png" alt="Logo" width={50} height={50} />
+            <Image src="/logo_color.png" alt="Logo" width={100} height={50} />
         </div>
         <div>
-            <ul className="flex gap-8 text-white text-2xl">
-                <li><a href="">Home</a></li>
-                <li><a href="">Event</a></li>
-                <li><a href="">About</a></li>
+            <ul className="flex gap-8 text-white text-lg ">
+                <li className="hover:text-blue-600"><a href="">Home</a></li>
+                <li className="hover:text-blue-600"><a href="">Event</a></li>
+                <li className="hover:text-blue-600"><a href="">About</a></li>
             </ul>
         </div>
     </div>
