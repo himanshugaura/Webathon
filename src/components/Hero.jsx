6bae8f5ec6a819c208e6717914e47c5628581577
himanshu <himanshu32505@gmail.com>
2025-08-20 13:32:53 +0530

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { BackgroundBeams } from './ui/background-beams'
 
 const Hero = () => {
   return (
-    <div className="h-screen fixed w-full">
+    <div className="h-screen w-full relative bg-black">
         <BackgroundBeams />
     </div>
   )
